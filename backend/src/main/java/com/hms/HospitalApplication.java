460c42a88e50ae2d429e6d1d50d538ba4f4821ed
srinathnaidu303 <srinathm030@gmail.com>
2026-03-30 11:08:09 +0530

    fixed railway crash config

--- backend/src/main/java/com/hms/HospitalApplication.java
+++ backend/src/main/java/com/hms/HospitalApplication.java
@@ -3,11 +3,7 @@ package com.hms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = {
-        org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
-        org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class,
-        org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration.class
-})
+@SpringBootApplication // ✅ NO EXCLUDES
 public class HospitalApplication {
 
     public static void main(String[] args) {
